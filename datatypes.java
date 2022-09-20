9b80f8d0b493c86de86806017fbe3738035d8f20
ELİF GİDER <egider20@gmail.com>
2022-09-21 00:40:47 +0300

    ilk 7 ders

---
+++ datatypes.java
@@ -0,0 +1,21 @@
+public class datatypes {
+    
+    public static void main(String[] args) {
+    
+        //boolean 1 byte
+        //char    2 bytes
+        //byte    1 byte
+        //short   2 bytes
+        //int     4 bytes
+        //long    8 bytes
+        //float   4 bytes
+        //double  8 bytes
+
+        double sayi=12.5;
+        sayi=-129;
+        char karakter='A';
+        boolean dogruMu=false;
+        
+
+    }
+}
---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+//reusability=bir kere yaz her yerde kullan!
+    public static void main(String[] args) {
+        
+        int ogrenciSayisi=10;
+        String mesaj="Öğrenci sayısı:";
+        System.out.println(mesaj+ogrenciSayisi);
+    }
+
+}
